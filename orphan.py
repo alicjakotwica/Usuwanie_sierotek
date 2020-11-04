6ba8ac846e63f0e9dec8c5861d9f0bbde67849e5
Robert Galat <robgal519@gmail.com>
2020-11-04 21:21:40 +0100

    new tests, added readme, fix multiple orphan case, added finding tex files

--- orphan.py
+++ orphan.py
@@ -1,15 +1,25 @@
 import re
-import sys
+import glob
 
-def remove_orphants(line) -> str:
-    return re.sub("(\s[iaezw])\s", "\g<1>~", line)
+def remove_orphans(line) -> str:
+    # Begin of line or space, any letter, soft space
+    orphans_pattern = r"(^|[ ~])([a-zA-Z],?) "
+    result = line
+    for i in range(2):
+        # need to run twice, in case of sentence like "a b c dasdf" 
+        # it will be fixed to "a~b c~dasdf", and second run will clean the rest
+        result = re.sub(orphans_pattern, r"\g<1>\g<2>~", result)
+
+    return result
+
 
 if __name__ == "__main__":
-    for i in range(len(sys.argv)-1):
+    files_to_correct = glob.glob("*.tex")
+    for f in files_to_correct:
         lines=[]
-        with open(sys.argv[i+1],"r") as doc:
+        with open(f,"r") as doc:
             lines=doc.readlines()
-        with open(sys.argv[i+1],"w") as doc:
+        with open(f,"w") as doc:
             for line in lines:
-                doc.write(remove_orphants(line))
+                doc.write(remove_orphans(line))
                 
--- orphan_test.py
+++ orphan_test.py
@@ -1,25 +1,34 @@
 import unittest
-import orhan
+import orphan
 
 class TestStringMethods(unittest.TestCase):
 
     def test_single_i(self):
-        self.assertEqual(orhan.remove_orphants("The rain i Spain"),"The rain i~Spain")
+        self.assertEqual(orphan.remove_orphans("The rain i Spain"),"The rain i~Spain")
 
     def test_double_i(self):
-        self.assertEqual(orhan.remove_orphants("The rain i Spain i fguyegf"),"The rain i~Spain i~fguyegf")
+        self.assertEqual(orphan.remove_orphans("The rain i Spain i fguyegf"),"The rain i~Spain i~fguyegf")
 
     def test_single_a(self):
-        self.assertEqual(orhan.remove_orphants("The rain a Spain"),"The rain a~Spain")
+        self.assertEqual(orphan.remove_orphans("The rain a Spain"),"The rain a~Spain")
     
     def test_single_w(self):
-        self.assertEqual(orhan.remove_orphants("The rain w Spain"),"The rain w~Spain")
+        self.assertEqual(orphan.remove_orphans("The rain w Spain"),"The rain w~Spain")
 
     def test_single_z(self):
-        self.assertEqual(orhan.remove_orphants("The rain z Spain"),"The rain z~Spain")
+        self.assertEqual(orphan.remove_orphans("The rain z Spain"),"The rain z~Spain")
 
     def test_single_e(self):
-        self.assertEqual(orhan.remove_orphants("The rain e Spain"),"The rain e~Spain")
+        self.assertEqual(orphan.remove_orphans("The rain e Spain"),"The rain e~Spain")
+        
+    def test_comma_after_orphant(self):
+        self.assertEqual(orphan.remove_orphans("The rain e, Spain"),"The rain e,~Spain")
+
+    def test_orphan_on_begin(self):
+        self.assertEqual(orphan.remove_orphans("I The rain Spain"),"I~The rain Spain")
+
+    def test_multiple_orphants(self):
+        self.assertEqual(orphan.remove_orphans("The rain i w e Spain"),"The rain i~w~e~Spain")
 
 if __name__ == '__main__':
     unittest.main()
